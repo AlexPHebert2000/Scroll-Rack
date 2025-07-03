6ba73362fe06f09218e1101e4bb3089c1f73356c
AlexPHebert2000 <aphebert9994@gmail.com>
2025-07-02 21:42:53 -0500

    FIX upload 1 chunk at a time to avoid connection limit

--- src/server/helper/scryfall-download.ts
+++ src/server/helper/scryfall-download.ts
@@ -4,55 +4,39 @@ import { PrismaClient } from '../../../generated/prisma/index.js';
 const CHUNK_SIZE = 1000;
 
 export default async () => {
+  
   console.time('Upload time');
+  
   const prisma = new PrismaClient();
+  
   const {data} = await axios.get('https://api.scryfall.com/bulk-data')
   const bulkData = (await axios.get(data.data[0].download_uri)).data.filter(card => card.layout !== 'art_series' && card.layout !== 'token');
-  const uploads = [];
-  await prisma.card.deleteMany({});
+  
+  await prisma.card.deleteMany({}); // Clear the card table before uploading new data
+  
   console.log(`Uploading ${bulkData.length} cards to the database...`);
+  
   for (let i = 0; i < bulkData.length; i += CHUNK_SIZE) {
     const chunk = bulkData.slice(i, i + CHUNK_SIZE);
     console.log(`Uploading cards ${i + 1} to ${Math.min(i + CHUNK_SIZE, bulkData.length)}...`);
     console.time(`Chunk ${i / CHUNK_SIZE + 1} upload time`);
-    /*
-    const upload = prisma.card.createMany({
-      data: chunk.map(card => ({
-        id: card.id,
-        name: card.name,
-        imageUrl: card.image_uris?.normal || null, // Use normal image if available, otherwise null
-      })),
-    })
-    .then(() => {
-      console.timeEnd(`Chunk ${i / CHUNK_SIZE + 1} upload time`);
-    })
-    .catch(error => {
-      console.error(`Error uploading chunk ${i / CHUNK_SIZE + 1}:`, error);
-    });
-    uploads.push(upload);
-    */
-    const upload = chunk.map((card) => {
-      return prisma.card.create({
-        data: {
-          id: card.id,
-          name: card.name,
-          imageUrl: card.image_uris?.normal || null, // Use normal image if available, otherwise null
-        },
-      });
-    })
-    uploads.push(Promise.all(upload).then(() => {console.timeEnd(`Chunk ${i / CHUNK_SIZE + 1} upload time`);}))
+
+    await Promise.allSettled(
+      chunk.map(card => 
+        prisma.card.upsert({
+          where: { id: card.id },
+          update:{},
+          create: {
+            id: card.id,
+            name: card.name,
+            imageUrl: card.image_uris?.normal || null,
+          }
+        })
+      )
+    )
   }
-  console.log(uploads.length, 'uploads created');
-  await Promise.allSettled(uploads)
-  .then((outcome) => {
-    const failedUploads = outcome.filter(result => result.status === 'rejected');
-    if (failedUploads.length > 0) {
-      console.error(`Failed to upload ${failedUploads.length} chunks.`);
-      console.error(failedUploads.map(failure => failure.reason));
-    } else {
-      console.log('All chunks uploaded successfully.');
-    }
-  });
-  await prisma.$disconnect();
+
   console.timeEnd('Upload time');
+  await prisma.$disconnect();
+  console.log('Database connection closed.');
 }
