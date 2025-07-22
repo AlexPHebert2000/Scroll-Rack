968395d2c8f9bc16613272b099d0264c58210386
Alex Hebert <alex.p.hebert.dev@gmail.com>
2025-07-21 21:39:22 -0500

    ADD user api endpoint

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import scryfallRouter from "./scryfall.js";
+import userRouter from "./user.js";
 
 
 
 const api = Router();
 api.use("/scryfall", scryfallRouter);
+api.use("/user", userRouter);
 
 export default api;
---
+++ src/server/api/user.ts
@@ -0,0 +1,6 @@
+import {Router} from "express";
+import type {Request, Response} from "express";
+
+const userRouter = Router();
+
+export default userRouter;
