aaf3ff7af8f03c3444da85114bc053de1765af78
AlexPHebert2000 <aphebert9994@gmail.com>
2025-09-22 13:07:31 -0500

    ADD set up react router

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,15 +1,24 @@
 import "./App.css";
-import { useState } from "react";
 import Box from '@mui/material/Box'
+import {Routes, Route} from 'react-router-dom';
+import type { ReactElement } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+function App() :ReactElement {
   return (
-    <Box>
-      Scroll Rack
-    </Box>
+    <>
+      <Box>
+        Scroll Rack
+      </Box>
+      <Routes>
+        {routes.map(({path, element}, index) => (
+          <Route key={path + index} path={path} element={element}/>
+        ))}
+      </Routes>
+    </>
   );
 }
+
+const routes : {path: string, element: ReactElement}[] = [
+]
 
 export default App;
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,12 +1,12 @@
 import "./index.css";
-
 import React from "react";
-import ReactDOM from "react-dom/client";
+import {BrowserRouter} from 'react-router-dom'
+import {createRoot} from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 );
