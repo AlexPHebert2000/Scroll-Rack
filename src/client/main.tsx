d0fc04d2ebcd4ca673f3fde675f1fd9a2ea68f30
AlexPHebert2000 <aphebert9994@gmail.com>
2025-09-22 21:18:52 -0500

    ADD react query setup

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,11 +2,16 @@ import "./index.css";
 import React from "react";
 import {BrowserRouter} from 'react-router-dom'
 import {createRoot} from "react-dom/client";
+import {QueryClient, QueryClientProcvider} from '@tanstack/react-query';
 
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProcvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </QueryClientProcvider>
 );
