b3b3d1ab591c3cf7b4707bd073f168d323f3abce
Alex <alexphebert@gmail.com>
2026-04-04 16:52:11 -0500

    ADD login redirect if no cookie found

--- src/client/components/Home.tsx
+++ src/client/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, {use, useState} from "react";
+import React, {useEffect, useState} from "react";
 import type { ReactElement } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
@@ -6,28 +6,29 @@ import { useNavigate } from "react-router-dom";
 import Cookie from "js-cookie";
 
 const Home = () :ReactElement => {
-
-
   const navigate = useNavigate();
-
   const checkCookie = async () => {
     const sessionCookie = Cookie.get("scroll-rack-session");
     return await axios.get(`/api/user/session/${sessionCookie}`);
   }
 
-  const {data} = useQuery({queryKey: ['sessionLookup'], queryFn: checkCookie})
-
-  const user : {username: string, decks: {name: string, id: string}[]} = data?.data.user;
+  const q = useQuery({queryKey: ['sessionLookup'], queryFn: checkCookie})
+  const user : {username: string, decks: {name: string, id: string}[]} = q.data?.data.user
 
   const handleClickDeck = (e : React.MouseEvent, id : string) => {
     navigate(`/deck?id=${id}`);
   }
+  
+  useEffect(() => {
+    if (!Cookie.get("scroll-rack-session")){ navigate('/login')}
+    console.log(user)
+  }, [])
 
   return (
     <>
       Welcome Home
       <hr/>
-      {user ? user.decks.map(({name, id}, index) => <a onClick={(e) => handleClickDeck(e, id)} key={name+ index}>{name}</a>) : <></>}
+      {user && user.decks ? user.decks.map(({name, id}, index) => <a onClick={(e) => handleClickDeck(e, id)} key={name+ index}>{name}</a>) : <>No decks</>}
     </>
   )
 }
